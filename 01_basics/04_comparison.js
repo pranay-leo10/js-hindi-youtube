4667a567945dbb73461dd852acf871a07cacc576
pranay-leo10 <153218253+pranay-leo10@users.noreply.github.com>
2024-09-13 18:08:33 +0000

    comarisons done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1); 
+// console.log("2" == 2); // == => converts dataType ans then checks it
+// console.log("02" > 1);
+
+
+// console.log(null > 0) 
+// console.log(null == 0) 
+// console.log(null >= 0) 
+
+// console.log(undefined == 0)  
+// console.log(undefined > 0) 
+// console.log(undefined < 0) 
+
+/// '===' >= checks datatypes of values being comparison
+console.log(2 === 2);
+console.log("2" === 2);
+
+
