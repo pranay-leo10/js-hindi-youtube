1d799ef48c65364c391de94ce8ad6f244378a81e
pranay-leo10 <153218253+pranay-leo10@users.noreply.github.com>
2024-09-13 08:07:23 +0000

    container config done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"user strict" // treat all JS code as newer version 
+// alert(3+3)  gives error bcz we are using nodeJs , not a browser
+
+console.log(3+3);
+console.log("Pranay");
+
+//Primitive datatypes :-
+
+let name = "Pranay" //String ""
+let age = 18 //Number => 2 to power 52 or 53.....bigint for large numbers
+let isLoggedIn = true //boolean
+
+//null => standalone value
+//undefined => variable is declare but value is not given to it
+//symbol => to find the uniqueness of components
+
+
+//object
+console.log(typeof null);
+console.log(typeof undefined);
+
