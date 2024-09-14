11849b147e012f36da493f68c89e950cc684d0e2
pranay-leo10 <153218253+pranay-leo10@users.noreply.github.com>
2024-09-14 07:44:09 +0000

    String

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,39 @@
+const name = "Pranay"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+// console.log(`My name is ${name} and my repo count is ${repoCount}`);
+
+const newGame = new String('pranay-gupta-com') // another way to declare a string
+// console.log(newGame[0])
+// console.log(newGame.__proto__)
+// console.log(newGame.length)
+// console.log(newGame.toUpperCase())
+// console.log(newGame.charAt(5))
+// console.log(newGame.indexOf('y'))
+
+const newString = newGame.substring(0,4)
+// console.log(newString);
+
+const anotherString = newGame.slice(-12, 10)
+// console.log(anotherString);
+
+const newStringOne = "       Pranay    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim())
+
+const url = "https://pranay.com/pranay%20gupta"
+// console.log(url)
+// console.log(url.replace('%20', '-'));
+// console.log(url.includes('pranay'));
+// console.log(url.includes('prandev'));
+
+// console.log(newGame.split('-'));
+
+
+
+
+
+
+
