3139051cec48649114425a923cc2bfbd6b740e00
pranay-leo10 <153218253+pranay-leo10@users.noreply.github.com>
2024-09-15 12:38:36 +0000

    scopes 2

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,9 +4,49 @@ let a = 300
 if (true) {
     let a = 10
     const b = 20
-    console.log(`Inner: ${a}`)
+    // console.log(`Inner: ${a}`)
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "Pranay"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+        
+    }
+    // console.log(website);
+    // two()
+}
+// one()
+
+if (true) {
+    const username = "Pranay"
+    if (username === "Pranay") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+// +++++++++++++++++++++++++++++++++++++++ interesting example ++++++++++++++++++++++++++
+
+function addOne(num){
+    return num+1
+}
+
+addOne(5)
+
+
+const addTwo = function(num){
+    return num+2
+}
+console.log(addTwo(5))
+
+
+
