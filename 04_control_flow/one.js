138fa28847e6031b0d5e6857ad3b9ad0afab779f
pranay-leo10 <153218253+pranay-leo10@users.noreply.github.com>
2024-09-17 10:15:38 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,43 @@
+// if
+
+const isUserLoggedIn = true
+const temperature = 41
+
+// if(2 != 3){
+//     console.log("Executed");
+// }
+
+// if (temperature < 50) {
+//     console.log(`less than 50`);
+// }else{
+//     console.log(`temperature is greater than 50`)
+// } 
+// console.log(`executed`);
+
+// const score = 200
+// if (score > 100) {
+//     var power = "fly"
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+// console.log(`User power: ${power}`);
+
+
+// const balance = 1000
+// if(balance > 500) console.log("test");
+
+
+// const balance = 1000
+// if (balance < 500) {
+//     console.log("less than 500")
+// } else if(balance < 750){
+//     console.log("less than 750");
+// } else if (balance < 900) {
+//     console.log("less than 900");
+// }else{
+//     console.log("less than 1200");
+// }
+
+
+
+
